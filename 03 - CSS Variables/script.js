4d11c333d0b1a1264528afac8f79dfd2f412c6ed
cmatzenbach <cmatzenbach@gmail.com>
2017-01-06 01:57:37 -0500

    Finished challenge #3

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,10 +1,13 @@
 var elms = document.getElementsByTagName('input');
 
 for (var i = 0, len = elms.length; i < len; i++) {
-    elms[i].addEventListener('change', handleChange);
-    elms[i].addEventListener('mousemove', handleChange);
+  elms[i].addEventListener('change', handleChange);
+  elms[i].addEventListener('mousemove', handleChange);
+  console.log(elms[i]);
 }
+console.log(elms);
 
 function handleChange () {
-  console.log('working');
+  var suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
